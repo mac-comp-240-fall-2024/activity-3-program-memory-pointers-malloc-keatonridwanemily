6eef8e8e1f5593699dba41f5fff8b8323387b7ba
klawson3 <klawson3@mscs2024.olri.macalester.edu>
2024-09-11 20:23:50 +0000

    not done

--- activity3.c
+++ activity3.c
@@ -18,10 +18,10 @@ void task3();
 
 int main() {
     // Task 1: call the first example to show how a pointer to int works
-    // task1();
+   //task1();
 
     // Task 2: call the second example that creates static and dynamic arrays
-    task2();
+   // task2();
 
     // Task 3: call the last example, which does string operations
     // task3();
